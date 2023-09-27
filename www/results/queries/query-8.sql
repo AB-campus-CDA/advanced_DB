acd8595a55e419619b29c224dd9470ebfc86438f
AurelBouchard <au.bouchard@gmail.com>
2023-09-27 10:20:13 +0200

    correct query 8 => having

--- www/results/queries/query-8.sql
+++ www/results/queries/query-8.sql
@@ -1,10 +1,8 @@
-SELECT * from (
-                  SELECT number, SUM(order_product.quantity * price) AS total
-                  FROM order_product
-                           JOIN products
-                                ON order_product.product_id = products.id
-                           JOIN orders
-                                ON order_product.order_id = orders.id
-                  GROUP BY order_id
-              ) as all_orders
-WHERE total BETWEEN 100 AND 550;
+SELECT number, SUM(order_product.quantity * price) AS total
+FROM order_product
+         JOIN products
+              ON order_product.product_id = products.id
+         JOIN orders
+              ON order_product.order_id = orders.id
+GROUP BY order_id
+HAVING total BETWEEN 100 AND 550;
