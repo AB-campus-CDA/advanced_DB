d39121f0e1ff5ec63518dd89d5796b5da6fe3b42
AurelBouchard <au.bouchard@gmail.com>
2023-09-27 12:07:52 +0200

    test 4 WIP

---
+++ www/results/Iteration 2/test4.sql
@@ -0,0 +1,3 @@
+# à l'insertion d'une ligne avec un nom de client dans la table 'bill" (ligne de type "customer")
+# un trigger ajoute une ligne avec le nom du produit et le montant total pour chaque ligne de 'bought_last_month (ligne de type "bought")
+
