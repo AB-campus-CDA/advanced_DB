6853575ff19081c5a3698718ddda979036ca2732
AurelBouchard <au.bouchard@gmail.com>
2023-09-27 15:12:37 +0200

    test 6

---
+++ www/results/Iteration 2/test6.sql
@@ -0,0 +1,15 @@
+SELECT bill_id, euro_cache, employee, 'Amsterdam'
+FROM bills_amsterdam
+UNION ALL
+SELECT bill_id, euro_cache, employee, 'Antwerpen'
+FROM bills_antwerpen
+UNION ALL
+SELECT bill_id, euro_cache, employee, 'Groningen'
+FROM bills_groningen
+UNION ALL
+SELECT bill_id, euro_cache, employee, 'Haarlem'
+FROM bills_haarlem
+UNION ALL
+SELECT bill_id, euro_cache, employee, 'Leiden'
+FROM bills_leiden
+ORDER BY bill_id;
